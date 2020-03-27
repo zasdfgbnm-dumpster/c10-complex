8997d4d9f537a3293fccf701deb3a05ea52d0aba
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-03-26 23:46:34 -0700

    refactor test

--- c10/test/util/cuda_complex_test.cu
+++ c10/test/util/cuda_complex_test.cu
@@ -1,1 +1,14 @@
-#include <c10/test/util/complex_test.cpp>
+#include <c10/test/util/complex_test_common.h>
+
+__managed__ bool pass;
+
+TEST(NonStaticHostTests, all) {
+  constructors::test_thrust_conversion();
+  assignment::test_assign_thrust();
+  io::test_io();
+  test_std::test_values();
+}
+
+int main() {
+  NonStaticHostTests_all();
+}
